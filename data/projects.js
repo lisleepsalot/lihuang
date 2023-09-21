13fe9eb930136bb4c4df8a91f77c894ef966ad5d
lisleepsalot <lhuang02@risd.edu>
2023-09-21 14:19:24 -0400

    sidbar created

--- data/projects.js
+++ data/projects.js
@@ -1,7 +1,19 @@
 const projects =[
   {
     image: "images/grid-covers/cover1.png",
-    name: "Project name"
+    name: "Hospital Navigation"
+  },
+  {
+    image: "images/grid-covers/cover1.png",
+    name: "Puzzle Type"
+  },
+  {
+    image: "images/grid-covers/cover1.png",
+    name: "After Party Zine"
+  },
+  {
+    image: "images/grid-covers/cover1.png",
+    name: "Anatomy of Fascism"
   },
   {
     image: "images/grid-covers/cover1.png",
@@ -31,6 +43,9 @@ const projects =[
     image: "images/grid-covers/cover1.png",
     name: "Project name"
   },
-
+  {
+    image: "images/grid-covers/cover1.png",
+    name: "Project name"
+  },
   
 ]
