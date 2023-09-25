bba779a93146703beee3da2ce92f44e9cd7adc40
lisleepsalot <lhuang02@risd.edu>
2023-09-24 21:38:00 -0400

    added categories in products

--- data/projects.js
+++ data/projects.js
@@ -1,26 +1,29 @@
 const projects =[
   {
-    image: "images/grid-covers/cover1.png",
-    name: "Hospital Navigation"
+    image: "images/covers-gif/after-party.gif",
+    name: "After Party Zine",
+    categories: ['editorial','design'],
   },
   {
-    image: "images/grid-covers/cover1.png",
-    name: "Puzzle Type"
+    image: "images/covers-gif/anatomy-of-fascism.gif",
+    name: "Puzzle Type",
+    categories: ['type','web'],
   },
   {
-    image: "images/grid-covers/cover1.png",
+    image: "images/covers-gif/hope-and-anchor.gif",
     name: "After Party Zine"
   },
   {
-    image: "images/grid-covers/cover1.png",
+    image: "images/covers-gif/anatomy-of-fascism.gif",
     name: "Anatomy of Fascism"
+
   },
   {
-    image: "images/grid-covers/cover1.png",
+    image: "images/covers-gif/hope-and-anchor.gif",
     name: "Project name"
   },
   {
-    image: "images/grid-covers/cover1.png",
+    image: "images/covers-gif/after-party.gif",
     name: "Project name"
   },
   {
