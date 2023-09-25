bf5a4ee0aed160adcd5eed730418b4ee9dd6d60b
lisleepsalot <lhuang02@risd.edu>
2023-09-25 03:03:34 -0400

    styled header icons

--- scripts/data/projects.js
+++ scripts/data/projects.js
@@ -8,36 +8,39 @@ const projects =[
   {
     image: "images/covers-gif/puzzle-type.gif",
     name: "Puzzle Type",
-    categories: ['type','web'],
+    categories: ['type','web','design'],
     identifier: 'puzzleType',
   },
   {
     image: "images/covers-gif/hope-and-anchor.gif",
     name: "Hope & Anchor Brandbook",
-    categories: ['branding','editorial'],
+    categories: ['branding','editorial','design'],
     identifier: 'hopeAndAnchor',
   },
   {
     image: "images/covers-gif/anatomy-of-fascism.gif",
     name: "Anatomy of Fascism",
-    categories: ['editorial'],
-
-
+    categories: ['editorial', 'design'],
+    identifier: 'anatomyOfFascism',
   },
   {
     image: "images/covers-gif/russian-avant-garde.png",
-    name: "Russian Avant-Garde"
+    name: "Russian Avant-Garde",
+    categories: ['design'],
+    identifier: 'russianAg',
   },
   {
     image: "images/covers-gif/hospital-navigation.gif",
-    name: "Hospital Navigation"
+    name: "Hospital Navigation",
+    categories: ['design'],
+    identifier: 'hospitalNavigation',
   },
   {
-    image: "images/grid-covers/cover1.png",
+    image: "images/covers-gif/puzzle-type.gif",
     name: "Project name"
   },
   {
-    image: "images/grid-covers/cover1.png",
+    image: "images/covers-gif/hospital-navigation.gif",
     name: "Project name"
   },
   {
