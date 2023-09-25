1c0845236083b05aa5e29afeb3ce7d212a3daa13
lisleepsalot <lhuang02@risd.edu>
2023-09-25 02:23:29 -0400

    implemented content images for h&a, ap

---
+++ scripts/hover.js
@@ -0,0 +1,9 @@
+const imageContainers = document.querySelectorAll('.image-container');
+
+// Add a click event listener to each "image-container" element
+imageContainers.forEach((container) => {
+  container.addEventListener('click', () => {
+    // Toggle the "bigger" class on the clicked element
+    container.classList.toggle('bigger');
+  });
+});
