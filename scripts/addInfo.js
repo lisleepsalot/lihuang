388c81be3b0ccd332128a840edc7570eec7c6435
lisleepsalot <lhuang02@risd.edu>
2023-09-26 03:40:49 -0400

    added info bar

--- scripts/addInfo.js
+++ scripts/addInfo.js
@@ -1,10 +1,13 @@
-let infoHTML = "";
-
-  projectsHTML +=
-  `
-  <div>Hi this is li</div>
-  `
-  document.querySelector('.top-icon').addEventListener('click', function() {
-    document.querySelector('.flex-container flex-1').
-  innerHTML = projectsHTML;
-  });
+document.querySelector('.top-icon').addEventListener('click', function() {
+  const addAboveElement = document.querySelector('.add-above');
+  
+  if (addAboveElement.innerHTML.trim() !== '') {
+    addAboveElement.innerHTML = ''; // Empty the content
+  } else {
+    addAboveElement.innerHTML = `
+      <div class="info">
+        Li is currently hard at work finishing this website. Be sure to check back soon for exciting updates and new features!
+      </div>
+    `;
+  }
+});
