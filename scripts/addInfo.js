5afbf1af412e8d7e4d3865fb6be82cfb6a96c4f7
lisleepsalot <lhuang02@risd.edu>
2023-09-26 03:05:24 -0400

    added add info.js

---
+++ scripts/addInfo.js
@@ -0,0 +1,10 @@
+let infoHTML = "";
+
+  projectsHTML +=
+  `
+  <div>Hi this is li</div>
+  `
+  document.querySelector('.top-icon').addEventListener('click', function() {
+    document.querySelector('.flex-container flex-1').
+  innerHTML = projectsHTML;
+  });
