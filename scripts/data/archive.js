f9ec5cec4b39dda1c778dede3b95308374c8474c
lisleepsalot <lhuang02@risd.edu>
2024-01-24 00:26:00 -0500

    updated archive text

--- scripts/data/archive.js
+++ scripts/data/archive.js
@@ -2,72 +2,72 @@ const archives =[
   {
     index:'yb',
     image: 'images/illustration/1.png',
-    description:'High School Yearbook, 2020'
+    description:'High School Yearbook<br>@Beijing No.4 High School, 2020'
   },
   {
     index:'mh',
     image: 'images/illustration/mh.png',
-    description:`Poster series designed for RISD Professor Min Hee Lee's portfolio sessions, 2023`
+    description:`Poster series designed for RISD Professor Min Hee Lee's portfolio sessions@RISD, 2023`
   },
   {
     index:'lt1',
     image: 'images/illustration/2.jpg',
-    description:'Mobile wallpaper for luckin tea, 2020'
+    description:'Mobile wallpaper for luckin tea<br>@Luckin Coffee, 2020'
   },
   {
     index:'cards',
     image: 'images/illustration/cards.png',
-    description:`Member card covers designed for Luckin Coffee, 2021`
+    description:`Member card covers designed for Luckin Coffee<br>@Luckin Coffee, 2021`
   },
   {
     index:'cloud',
     image: 'images/illustration/cloud.png',
-    description:`Poster designed for RISD Artist Ball <br> (RISD Design Guild Challenge), 2023`
+    description:`Poster designed for RISD Artist Ball <br>@RISD, 2023`
   },
   {
     index:'cook',
     image: 'images/illustration/cook1.png',
-    description:`Poster for 'To Cook___In Here' Project`
+    description:`Poster for 'To Cook___In Here' Project<br>@RISD, 2023`
   },
   {
     index:'lt2',
     image: 'images/illustration/10.gif',
-    description:`Mobile holiday wallpaper for luckin tea, 2020`
+    description:`Mobile holiday wallpaper for luckin tea <br> @Luckin Coffee, 2020`
   },
   {
     index:'donut',
     image: 'images/illustration/donut.png',
-    description:`Poster designed for 'Pardoy Music Festival' for Design Studio 1, 2022`
+    description:`Conceptual poster designed for 'Pardoy Music Festival' for Design Studio 1<br>@RISD, 2022`
   },
   {
     index:'hospital',
     image: 'images/illustration/hospital.png',
-    description:`Process sheets for 'Hospital Navigation' `
+    description:`Process sheets for 'Hospital Navigation'<br>@RISD, 2023 `
   },
   {
-    index:'pradoy',
+    index:'yellowCity',
     image: 'images/illustration/yellowCity.png',
-    description:`Poster designed for 'Pardoy Music Festival' for Design Studio 1, 2022`
+    description:`Conceptual poster designed for 'Yellow City Music Festival' for Typography II<br>@RISD, 2023`
   },
   {
     index:'charms',
     image: 'images/illustration/charms.png',
-    description:`Monthly social medai post for Luckin Coffee, 2021`
+    description:`Monthly social medai post for Luckin Coffee<br>@Luckin Coffee, 2021`
   },
   {
     index:'kira',
     image: 'images/illustration/kira.png',
-    description:`Identity design and peripheral design for KiraKira, a car detailing studio in Shanghai, 2021`
+    description:`Identity design and peripheral design for KiraKira, a car detailing studio in Shanghai<br>@JYCC Group, 2021`
   },
   {
     index:'russ',
     image: 'images/illustration/russ.png',
-    description:`Infographic designed for Russian Revolution Seminar at Brown, 2022`
+    description:`Infographic designed for Russian Revolution Seminar<br>@Brown University, 2022`
   },
   {
     index:'meowfia',
     image: 'images/illustration/pixels.png',
-    description:`Background art for the game Meowfia, 2022`
+    description:`Background art for the game Meowfia<br>@RISD, 2022`
   },
 
 
