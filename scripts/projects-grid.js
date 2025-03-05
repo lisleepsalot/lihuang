3bc073d167f06326ed21b263412b350c9631c71c
lisleepsalot <lhuang02@risd.edu>
2025-03-05 12:27:41 -0500

    cover images updates

--- scripts/projects-grid.js
+++ scripts/projects-grid.js
@@ -1,24 +1,35 @@
-function makeGrid(){
+function makeGrid() {
   let projectsHTML = "";
-  const currentHTML = document.querySelector('.projects-display-grid').
-  innerHTML;
-  
-  projects.forEach((project)=>{
+  const currentHTML = document.querySelector('.projects-display-grid').innerHTML;
 
-    projectsHTML +=
-    `
-    <div class="project-container js-${project.identifier}" onclick="goTo('${project.identifier}');">
-      <div style="position: relative">
-        <img class="grid-image" src="${project.image}" alt="${project.image}">
-      </div>
-      <p class="h1 project-preview-title">${project.name}</p>
-    </div>
-    `
-  })
-  
-  
-  document.querySelector('.projects-display-grid').
-  innerHTML = currentHTML + projectsHTML;
+  projects.forEach((project) => {
+      let mediaElement = ``;
+
+      // Check if the file is a video or an image
+      if (project.image.endsWith('.mp4')) {
+          mediaElement = `
+              <video class="grid-cover" autoplay muted loop playsinline>
+                  <source src="${project.image}" type="video/mp4">
+                  Your browser does not support the video tag.
+              </video>
+          `;
+      } else {
+          mediaElement = `
+              <img class="grid-cover" src="${project.image}" alt="${project.name}">
+          `;
+      }
+
+      projectsHTML += `
+          <div class="project-container js-${project.identifier}" onclick="goTo('${project.identifier}');">
+              <div style="position: relative; width: 100%">
+                  ${mediaElement}
+              </div>
+              <p class="h1 project-preview-title">${project.name}</p>
+          </div>
+      `;
+  });
+
+  document.querySelector('.projects-display-grid').innerHTML = currentHTML + projectsHTML;
   return currentHTML + projectsHTML;
 }
 
