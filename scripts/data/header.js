b9887fbd1264f6977d7679360eb13c233290d453
lisleepsalot <lhuang02@risd.edu>
2025-03-06 01:30:33 -0500

    updated a few projects!

---
+++ scripts/data/header.js
@@ -0,0 +1,76 @@
+const headerUpperHTML = `
+    <div class="header-upper">
+        <div class="header-text header-block">
+            <p style="margin: 0;">
+                <a class="removeA hover-yellow" href="index.html" target="_blank">Li Huang</a>
+                <div id="animation1">-->-->-->-->-->--> </div>
+                <span id="current-datetime"></span><br>
+                (last updated on 03/2025)<br> 
+            </p>
+        </div>
+        <div class="header-block">
+            <p class="header-text" style="margin: 0;">
+                <span id="title">Graphic Designer</span> <br>
+                Pursuing <a class="removeA hover-yellow" href="https://risd.gd/" target="_blank">BFA @RISD.GD</a>, <br>
+                Currently @<a class="removeA hover-yellow" href="https://www.risdguild.com/" target="_blank">RISD Design Guild</a> <br>
+                Previously @<a class="removeA hover-yellow" href="https://www.apple.com/" target="_blank">Apple</a>
+            </p>
+        </div>
+        <div class="header-block">
+            <p class="header-text" style="margin: 0;">
+                <a class="removeA hover-yellow" href="https://www.instagram.com/lihuang.space/" target="_blank">Instagram</a>, <br> 
+                <a class="removeA hover-yellow" href="https://www.linkedin.com/in/li-huang-7810231b8/" target="_blank">LinkedIn</a>, <br> 
+                <a class="removeA hover-yellow" href="mailto:lhuang02@risd.edu">E-mail</a>
+            </p>
+        </div>
+    </div>`;
+
+const pageHeaderUpperHTML = `
+    <div class="header-upper">
+        <div class="header-text header-block">
+            <p style="margin: 0;">
+                <a class="removeA hover-yellow" href="../index.html">Li Huang</a>
+            </p>
+        </div>
+        <div class="header-block">
+            <span class = "header-text" id="current-datetime"></span><br>
+        </div>
+        <div class="header-block">
+            <p class="header-text" style="margin: 0;">
+                <a class="removeA hover-yellow" href="https://www.instagram.com/lihuang.space/" target="_blank">Instagram</a>, 
+                <a class="removeA hover-yellow" href="https://www.linkedin.com/in/li-huang-7810231b8/" target="_blank">LinkedIn</a>,  
+                <a class="removeA hover-yellow" href="mailto:lhuang02@risd.edu">E-mail</a>
+            </p>
+        </div>
+    </div>`;
+
+const headerLowerHTML = `
+    <div class="header-lower header-text">
+        <div class="header-block">
+            <p class="underline-div">My projects</p>
+            <div class="type-checkbox">editorial</div>
+            <div class="type-checkbox">branding</div>
+            <div class="type-checkbox">type design</div>
+        </div>
+
+        <div class="header-block">
+            <p class="header-text underline-div">Display</p>
+            <div class="display-type">grid</div>
+            <div class="display-type">list</div>
+        </div>
+
+        <div class="header-block">
+            <p class="header-text underline-div">Random Buttons</p>
+            <div class="button-container">
+                <div class="question-button">i</div> 
+                <div class="info-button">?</div>
+                <div class="clear-button">c</div>
+            </div>
+            <div class="archive-animation">
+                <video class="animation-2" autoplay muted loop playsinline>
+                    <source src="images/other/animation2.mp4" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+        </div>
+    </div>`;
---
+++ scripts/pageload.js
@@ -0,0 +1,64 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const isIndexPage = window.location.pathname.endsWith("index.html") || window.location.pathname === "/";
+
+  // Insert header content
+  document.querySelectorAll(".header").forEach(header => {
+      if (isIndexPage) {
+          header.innerHTML = headerUpperHTML + headerLowerHTML;
+      } else {
+          header.innerHTML = pageHeaderUpperHTML;
+      }
+  });
+
+  // Remove border/outline if not index.html
+  if (!isIndexPage) {
+      document.querySelectorAll(".header-upper").forEach(headerUpper => {
+          headerUpper.style.border = "none";
+          headerUpper.style.outline = "none";
+      });
+  }
+});
+
+//this method blinks the text div
+let text = "hello!";
+let prefix = "------------"; 
+let fullString = text + prefix;
+let length = fullString.length;
+let index = 0;
+
+function animateText() {
+    let shiftedText = fullString.substring(length - index) + fullString.substring(0, length - index);
+    document.getElementById("animation1").textContent = shiftedText;
+
+    index = (index + 1) % length;
+}
+
+setInterval(animateText, 100); 
+
+//this method blinks my title
+function updateDateTime() {
+  let now = new Date();
+
+  let options = {
+      timeZone: "America/New_York",
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: false // 24-hour format
+  };
+
+  let estDateTime = new Intl.DateTimeFormat("en-US", options).format(now);
+
+  let dateTimeElement = document.getElementById("current-datetime");
+  if (dateTimeElement) {
+      dateTimeElement.textContent = estDateTime;
+  } else {
+      console.warn("Element #current-datetime not found.");
+  }
+}
+
+updateDateTime(); 
+setInterval(updateDateTime, 1000);
